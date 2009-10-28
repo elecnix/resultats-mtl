e432262d443780c1aab3f08d0cd95cf74c1eff39
Nicolas Marchildon <nicolas@marchildon.net>
2009-10-28 10:10:20 -0400

    Fix division par zéro

--- app/models/candidat_maire.rb
+++ app/models/candidat_maire.rb
@@ -3,6 +3,6 @@ class CandidatMaire < ActiveRecord::Base
   
   def pourcentage
     nb_voix_exprimees = CandidatMaire.find(:all).inject { |voix, maire| maire.nb_voix_obtenues }
-    nb_voix_obtenues / nb_voix_exprimees
+    nb_voix_obtenues / nb_voix_exprimees if nb_voix_exprimees > 0
   end
 end
