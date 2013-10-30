f38abea86d93fb02fe88dfca807e6c6eb53b5987
Nicolas Marchildon <nicolas@marchildon.net>
2013-10-29 21:11:34 -0400

    Reverse-engineer database schema from import script

---
+++ db/migrate/20131030000705_reverse_engineer.rb
@@ -0,0 +1,81 @@
+class ReverseEngineer < ActiveRecord::Migration
+  def up
+    create_table :sommaires do |t|
+      t.integer :nb_electeurs_inscrits
+      t.integer :nb_total_voix_recueillies
+      t.integer :nb_bulletins_rejetes
+      t.integer :nb_bureaux_depouilles
+      t.integer :nb_bureaux_total
+      t.float :taux_participation
+      t.integer :conseillers_de_ville_en_avance
+      t.string :date_heure_generation
+    end
+    create_table :arrondissements do |t|
+      t.integer :mtl_id
+      t.string :nom
+    end
+    create_table :partis do |t|
+      t.integer :mtl_id
+      t.string :nom
+      t.integer :postes_en_avance
+    end
+    create_table :districts do |t|
+      t.integer :mtl_id
+      t.string :nom
+    end
+    create_table :candidats do |t|
+      t.integer :mtl_id
+      t.string :nom
+      t.string :prenom
+      t.integer :nb_voix_obtenues
+      t.integer :nb_voix_majorite
+      t.integer :parti_id
+      t.integer :poste_id
+    end
+    create_table :candidat_maires do |t|
+      t.integer :mtl_id
+      t.string :nom
+      t.string :prenom
+      t.integer :nb_voix_obtenues
+      t.integer :nb_voix_majorite
+      t.integer :parti_id
+      t.integer :poste_id
+    end
+    create_table :candidat_maire_arrondissements do |t|
+      t.integer :mtl_id
+      t.integer :arrondissement_id
+      t.integer :parti_id
+      t.string :nom
+      t.string :prenom
+      t.integer :nb_voix_obtenues
+    end
+    create_table :postes do |t|
+      t.integer :mtl_id
+      t.string :mtl_type
+      t.integer :arrondissement_id
+      t.integer :district_id
+      t.string :nom
+      t.integer :nb_electeurs_inscrits
+      t.integer :nb_total_voix_recueillies
+      t.integer :nb_bulletins_rejetes
+      t.float :taux_participation
+      t.integer :nb_bureaux_depouilles
+      t.integer :nb_bureaux_total
+      t.integer :candidat_en_avance_id
+    end
+    create_table :affichages do |t|
+      t.integer :arrondissement_id
+    end
+  end
+
+  def down
+    drop_table :sommaires
+    drop_table :arrondissements
+    drop_table :partis
+    drop_table :districts
+    drop_table :postes
+    drop_table :candidats
+    drop_table :candidat_maires
+    drop_table :candidat_maire_arrondissements
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,81 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20131030000705) do
+
+  create_table "affichages", :force => true do |t|
+    t.integer "arrondissement_id"
+  end
+
+  create_table "arrondissements", :force => true do |t|
+    t.integer "mtl_id"
+    t.string  "nom"
+  end
+
+  create_table "candidat_maire_arrondissements", :force => true do |t|
+    t.integer "mtl_id"
+    t.integer "arrondissement_id"
+    t.integer "parti_id"
+    t.string  "nom"
+    t.string  "prenom"
+    t.integer "nb_voix_obtenues"
+  end
+
+  create_table "candidat_maires", :force => true do |t|
+    t.integer "mtl_id"
+    t.string  "nom"
+    t.string  "prenom"
+    t.integer "nb_voix_obtenues"
+    t.integer "nb_voix_majorite"
+    t.integer "parti_id"
+    t.integer "poste_id"
+  end
+
+  create_table "candidats", :force => true do |t|
+    t.integer "mtl_id"
+    t.string  "nom"
+    t.string  "prenom"
+    t.integer "nb_voix_obtenues"
+    t.integer "nb_voix_majorite"
+    t.integer "parti_id"
+    t.integer "poste_id"
+  end
+
+  create_table "districts", :force => true do |t|
+    t.integer "mtl_id"
+    t.string  "nom"
+  end
+
+  create_table "partis", :force => true do |t|
+    t.integer "mtl_id"
+    t.string  "nom"
+    t.integer "postes_en_avance"
+  end
+
+  create_table "postes", :force => true do |t|
+    t.integer "mtl_id"
+    t.string  "mtl_type"
+    t.integer "arrondissement_id"
+    t.integer "district_id"
+    t.string  "nom"
+    t.integer "nb_electeurs_inscrits"
+    t.integer "nb_total_voix_recueillies"
+    t.integer "nb_bulletins_rejetes"
+    t.float   "taux_participation"
+    t.integer "nb_bureaux_depouilles"
+    t.integer "nb_bureaux_total"
+    t.integer "candidat_en_avance_id"
+  end
+
+  create_table "sommaires", :force => true do |t|
+    t.integer "nb_electeurs_inscrits"
+    t.integer "nb_total_voix_recueillies"
+    t.integer "nb_bulletins_rejetes"
+    t.integer "nb_bureaux_depouilles"
+    t.integer "nb_bureaux_total"
+    t.float   "taux_participation"
+    t.integer "conseillers_de_ville_en_avance"
+    t.string  "date_heure_generation"
+  end
 
 end
